a981633323babce2d7e99c758191b8f351b57d4e
Mobin <warrierdragon30@gamil.com>
2025-10-16 17:59:49 +0330

    ignore .history folder

--- .history/frontend/src/pages/AuthPaga_20251016151711.jsx
+++
@@ -1,9 +0,0 @@
-import AuthCard from "../";
-
-export default function Auth() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <AuthCard />
-        </div>
-    );
-}
--- .history/frontend/src/pages/AuthPaga_20251016151718.jsx
+++
@@ -1,9 +0,0 @@
-import AuthCard from "../components/auth/AuthCard";
-
-export default function Auth() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <AuthCard />
-        </div>
-    );
-}
